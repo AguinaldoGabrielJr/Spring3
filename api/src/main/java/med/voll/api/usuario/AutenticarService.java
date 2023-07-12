9551c0432275beeb95aa80b88a4abe5ef51dbfed
Aguinaldo Gabriel dos Santos Junior <guina.santos.cwb@gmail.com>
2023-07-12 17:07:14 -0300

    Repository e Service

---
+++ api/src/main/java/med/voll/api/usuario/AutenticarService.java
@@ -0,0 +1,18 @@
+package med.voll.api.usuario;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticarService implements UserDetailsService {
+
+	private UsuarioRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return repository.findByLogin(username);
+	}
+
+}
---
+++ api/src/main/java/med/voll/api/usuario/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package med.voll.api.usuario;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	UserDetails findByLogin(String login);
+
+}
